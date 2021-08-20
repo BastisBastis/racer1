458ddba384e544366c78a87125bc31efca3a90e5
BastisBastis <65193082+BastisBastis@users.noreply.github.com>
2021-08-20 13:09:14 +0200

    Update Road.js

--- src/objects/Road.js
+++ src/objects/Road.js
@@ -4,12 +4,60 @@ const angleToRad = (angle) => {
   return angle * Math.PI / 180; 
 }
 
-class roadPoints {
-  constructor(x, y, width) {
+class RoadPoints {
+  constructor(x, y, width, dir) {
     this.outer = [x, y];
     this.inner = [x, y+width];
+    this.roadWidth = width;
+    this.currDir = dir;
   }
   
+  addStraight(dist) {
+    const newX1 = this.outer[0] + dist * Math.cos(angleToRad(this.currDir));
+    const newY1 = this.outer[1] + dist * Math.sin(angleToRad(this.currDir));
+    const newX2 = newX1 + this.roadWidth*Math.cos(angleToRad(this.currDir+90));
+    const newY2 = newY1 + this.roadWidth*Math.sin(angleToRad(this.currDir+90));
+    this.outer = [
+      newX1,
+      newY1,
+      ...this.outer
+    ];
+    this.inner = [
+      newX2,
+      newY2,
+      ...this.inner
+    ];
+  }
+  
+  addTurn(turnDir,stepDist, steps, stepAngle) {
+    for (let i = 0; i < steps; i++) {
+      let newX1, newX2, newY1, newY2;
+      if (turnDir === "l") {
+        this.currDir = (this.currDir - stepAngle + 360) % 360
+        newX1 = this.outer[0] + stepDist * Math.cos(angleToRad(this.currDir));
+        newY1 = this.outer[1] + stepDist * Math.sin(angleToRad(this.currDir));
+        newX2 = newX1 + this.roadWidth*Math.cos(angleToRad(this.currDir+90));
+        newY2 = newY1 + this.roadWidth*Math.sin(angleToRad(this.currDir+90));
+      }
+      else if (turnDir === "r") {
+        currDir = (currDir + stepAngle) % 360
+        newX2 = this.inner[0] + stepDist * Math.cos(angleToRad(this.currDir));
+        newY2 = this.inner[1] + stepDist * Math.sin(angleToRad(this.currDir));
+        newX1 = newX2 + this.roadWidth*Math.cos(angleToRad(this.currDir-90));
+        newY1 = newY2 + this.roadWidth*Math.sin(angleToRad(this.currDir-90));
+      }
+      this.outer = [
+      newX1,
+      newY1,
+      ...this.outer
+    ]
+    this.inner = [
+      newX2,
+      newY2,
+      ...this.inner
+    ]
+    }
+  }
   
 }
 
@@ -41,6 +89,7 @@ export default class Road extends Phaser.GameObjects.Polygon {
     const startX = 150;
     const startY = 200;
     
+    const roadPoints = new RoadPoints(startX, startY, roadWidth, currDir);
     
     let roadParts = [startX,startY] //50 = width of road
     
@@ -108,9 +157,11 @@ export default class Road extends Phaser.GameObjects.Polygon {
     addTurn("r",5,10,9);
     addStraight(50);
     addTurn("r",5,10,9);
-    addStraight(39)
+    addStraight(39);
+    
+    roadPoints.addStraight(200);
 
-    return new Road(scene,0,0, roadParts,innerRoad, 0x888888, 0x337733);
+    return new Road(scene,0,0, roadPoints.outer,roadPoints.inner, 0x888888, 0x337733);
   }
   
 }
